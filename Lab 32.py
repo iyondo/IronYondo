7f1ba42dd3c22cb7ae1391fdbc2f41dcf7e866c1
iyondo <93102402+iyondo@users.noreply.github.com>
2021-12-29 17:41:39 +0100

    Add files via upload

--- Lab 32.py
+++ Lab 32.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 Method:
-1 - Check the columns with missing values
-2 - Replace this missing values by 0
-3 - Check if there is any duplicate
-4 - Check the total amount per client
-5 - Encoding with Get Dummies - Profession
-6 - Encoding with LabelBinazer - BirthYear
-# In[1]:
+1 - Provide the descriptive statistics for the dataset
+2 - Check the columns with missing values and their type
+3 - Replace this missing values by 0
+4 - Check if there is any duplicate
+5 - Clean the matrice with mistakes
+6 - Matrix for correlation
+7 - Check the total amount per client
+8 - Encoding with Get Dummies - Profession
+9 - Encoding with LabelBinazer - BirthYear
+# In[ ]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[2]:
+# In[ ]:
 
 
 # import data set 
@@ -22,7 +25,15 @@ data =pd.read_csv(r'C:\Users\Ingrid\Desktop\DAFT Nov 21\Labs done\Labs module 1\
 data
 
 
-# In[3]:
+# In[ ]:
+
+
+#Provide the descriptive statistics for the dataset
+stat=data.describe()
+stat
+
+
+# In[ ]:
 
 
 # sum method to total up the number of True values(missing values) by column
@@ -31,19 +42,7 @@ null_cols[null_cols > 0]
 
 
 
-# In[4]:
-
-
-data['BirthYear'].dtype
-
-
-# In[5]:
-
-
-data['Profession'].dtype
-
-
-# In[6]:
+# In[ ]:
 
 
 # looks like the BirtYear column and the Profession column have a similar number of nulls
@@ -56,7 +55,19 @@ null_displ = null_displ[['TransactionID', 'ClientID', 'BirthYear', 'Amount', 'Pr
 null_displ
 
 
-# In[7]:
+# In[ ]:
+
+
+data['Profession'].dtype
+
+
+# In[ ]:
+
+
+data['BirthYear'].dtype
+
+
+# In[ ]:
 
 
 # Investigation by subsetting the data set and looking at just the records where Profession is null and just the columns we think will be informative in allowing us to determine a reason
@@ -69,7 +80,15 @@ null_displ
 # Not a duplicate for clientID|35008 - 2 different transactions
 
 
-# In[8]:
+# In[ ]:
+
+
+# Imputation using (Zero/Constant) Values:
+data = data.fillna(0)
+data
+
+
+# In[ ]:
 
 
 # Finding and Removing Duplicates: the drop_duplicates method. 
@@ -83,15 +102,7 @@ print('Number of duplicate records dropped: ', str(before - after))
 # there were no records that matched exactly across all columns.
 
 
-# In[9]:
-
-
-# Imputation using (Zero/Constant) Values:
-data = data.fillna(0)
-data
-
-
-# In[10]:
+# In[ ]:
 
 
 # We notice that in some cases the first letter is not a capital letter or there is mistake in the spelling 
@@ -111,13 +122,21 @@ data['Profession'] = data['Profession'].str.replace('etudient', 'Student')
 print(set(data['Profession']))
 
 
-# In[11]:
+# In[ ]:
 
 
 data
 
 
-# In[12]:
+# In[ ]:
+
+
+#Correlation matrix
+cor=data.corr()
+cor
+
+
+# In[ ]:
 
 
 # Total amount per client
@@ -125,7 +144,7 @@ data1 = data.groupby('ClientID').agg({'Amount': 'sum'})
 data1
 
 
-# In[13]:
+# In[ ]:
 
 
 # Encoding: 1st method - Get Dummies on column Profession,
@@ -133,7 +152,7 @@ y = pd.get_dummies(data.Profession, prefix = 'Profession') #create a dummie DF u
 print(y.head())
 
 
-# In[14]:
+# In[ ]:
 
 
 #2nd method - on column BirthYear
@@ -141,4 +160,10 @@ print(y.head())
 from sklearn.preprocessing import LabelBinarizer #import method
 y = LabelBinarizer().fit_transform(data.BirthYear) #initialization of method
 y
+
+
+# In[ ]:
+
+
+
 
